909f7d11183ced26717e6744f0ee98fb5064b1b5
Alex Black <alex@alexblack.ca>
2012-03-28 10:44:24 -0400

    added solution

---
+++ async_wait.js
@@ -0,0 +1,15 @@
+function async_wait() {
+  var fns = arguments; // capture
+  return {
+    then: function(done) {
+      var completed = 0;
+      for (var i = 0; i < fns.length; i++) {
+        fns[i](function() {
+          if (++completed == fns.length)
+            done();
+        });
+      }
+    }
+  };
+}
+
